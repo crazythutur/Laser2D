2782bbff83e481ad301500778145a467cceca819
Vallet Arthur <arthur.vallet@etu.umontpellier.fr>
2018-10-25 16:39:47 +0200

    Electron_presence update + doxygen integration

--- inc/Electron_presence.h
+++ inc/Electron_presence.h
@@ -1,3 +1,22 @@
+/**
+ * @class Electron_presence
+ *
+ * @author Arthur Vallet
+ *
+ * @version 1.0
+ *
+ * @date $Date: 2018/10/25$
+ *
+ *
+ * @brief The Electron_presence class represent the probability of finding an electron at different level for a @a QW_elementary_laser
+ * for different number of electron in Conduction Band (CB)
+ * this probability is deffined for each lasing mode by  by @a probability[CB_Electron_number][mode]
+ * or for pumping level by @a probability[CB_Electron_number][PUMPING_LEVEL]
+ * the @a mode_number
+ * @a energy_level_number  represent the maximum electron capacity of the CB that correspond to the energy level number
+ */
+
+
 #ifndef ELECTRON_PRESENCE_H
 #define ELECTRON_PRESENCE_H
 
@@ -6,31 +25,125 @@
 
 class Electron_presence
 {
-
+   /************************/
+   /*Constructor/Destructor*/
+   /************************/
 public:
-   Electron_presence(unsigned int const mode_number, std::size_t const energy_level_number,
-                     unsigned int const *laser_levels, double const q);
+   Electron_presence(unsigned int const *laser_levels, double const q);
 
-void affiche(std::ostream &flux) const;
+   ~Electron_presence();
 
-//void initialise_size(std::size_t const mode_number, std::size_t const energy_level_number);
+   /*****************/
+   /*Getters/Setters*/
+   /*****************/
 
-~Electron_presence();
+   unsigned int getMode_number() const
+   {
+      return mode_number;
+   }//unsigned int Electron_presence::getMode_number
 
-unsigned int getMode_number() const;
 
-unsigned int getEnergy_level_number() const;
+   unsigned int getEnergy_level_number() const
+   {
+      return energy_level_number;
+   }//unsigned int Electron_presence::getEnergy_level_number
 
-double **getProbability() const;
+
+   double **getProbability() const
+   {
+      return probability;
+   }//double **Electron_presence::getProbability
+
+   /******************/
+   /*Public functions*/
+   /******************/
+
+   /**
+   * @brief affiche print the electron presence tab
+   * @param flux
+   */
+   void affiche(std::ostream &flux) const;
+
+
+   /**
+    * @brief init_size initialise the size of the @a probability tab and so the static parameters if the class
+    * @param mode_number number of lasing modes
+    * @param energy_level_number represent the maximum electron capacity of the CB that correspond to the energy level number
+    */
+   static void initialize_size(unsigned int const mode_number, unsigned int const energy_level_number);
+
+   /*********************/
+   /*Public attributes*/
+   /*********************/
+   /**
+    * @brief PUMPING_LEVEL permit to obtain the probability of having an electron with @a probability[CB_Electron_number][PUMPING_LEVEL]
+    */
+   static unsigned int PUMPING_LEVEL;
+
+//============================================================================================================================
 
 private:
-double **probability;
-unsigned int mode_number;
-unsigned int energy_level_number;
+   /*********************/
+   /*Privates attributes*/
+   /*********************/
+   /**
+   * @brief probability probability of finding an electron at different level for a @a QW_elementary_laser
+   * for different number of electron in Conduction Band (CB)
+   * this probability is deffined for each lasing mode by  by @a probability[CB_Electron_number][mode]
+   * or for pumping level by @a probability[CB_Electron_number][PUMPING_LEVEL]
+   */
+   double **probability;
+   /**
+    * @brief mode_number the number of mode in the laser
+    */
+   static unsigned int mode_number;
+   /**
+    * @brief energy_level_number represent the maximum electron capacity of the CB that correspond to the energy level number
+    */
+   static unsigned int energy_level_number;
 
+
+   /**
+    * @brief initialise_occuancies inititialise the electron presence probability for each lasing mode and puming level
+    * for modes by by @a probability[][mode]and for the pumping lvl accecible by @a probability[][PUMPING_LEVEL]
+    * @param laser_levels
+    * @param q
+    */
+
+   /*******************/
+   /*Private functions*/
+   /*******************/
+   /**
+   * @brief initialise_occuancies permit to obtain the probability to find an electron in @a probability
+   *
+   * at laser lvl  with electron in BC done for each mode
+   * We use the fabrice formula that is iterative formula we calculate the probability table in two times
+   * because of the error propagation.
+   * the general formule for an occupancy (at stationary state) at level k is
+   * \f$ occ_k(N+1) = (q^k (1-occ_k(N)) \frac{1-q^{N+1}}{q^N-q^B}\f$
+   *
+   * see Vallet, A., Chusseau, L., Philippe, F., & Jean-Marie, A. (2019).
+   * Markov model of quantum fluctuations at the transition to lasing of semiconductor nanolasers.
+   *  Physica E: Low-dimensional Systems and Nanostructures, 105, 97-104.
+   *
+   * we can use the symetry of the band \f$occ_k(N)=1-occ_{B-1-k}(B-N)\f$
+   *
+   * to obtain
+   * \f$ occ_k(B-N-2) = 1-q^{B-1-k}occ_k(B-N-1)\frac{1-q^{N+1}}{q^N-q^B}\f$
+   *
+   * B is abreviation for @a energy_level_number
+   * k is abreviation for @a laser_levels in CB, NB : their is the same number of laser level in CB than modes numbers
+   * N is abreviation for the energy level we use this incrementation variable for loop all the energy level
+   *
+   * all result are contain in @a probability
+   * first dimention for witch number of electrons in CB, and the second dimention is for each laser mode and the pumping level.
+   * @param laser_levels table that contain all laser level for each mode
+   * @param q represent the temperature at exponential form q=exp(-E/(KbT)), E is the energy lvl between two lvl
+                   in our unity E=1meV, Kb is Boltzmann constant 1,38064852.10^-23 and T temperature in °K
+    */
    void initialise_occuancies(unsigned int const *laser_levels, double const q) ;
 
 
-};
+};// end of class Electron_presence
 
 #endif // ELECTRON_PRESENCE_H
--- src/Electron_presence.cpp
+++ src/Electron_presence.cpp
@@ -2,146 +2,79 @@
 
 using namespace std;
 
+unsigned int Electron_presence::energy_level_number;
+unsigned int Electron_presence::mode_number;
+unsigned int Electron_presence::PUMPING_LEVEL;
 
 
-/**
- * @brief get_occupancy permit to obtain the probability to find an electron
- *
- * at laser lvl  with electron in BC done for each mode
- * We use the fabrice formula that is iterative formula we calculate the probability table in two times
- * because of the error propagation.
- * the general formule for an occupancy (at stationary state) at level k is
- * \f$ occ_k(N+1) = (q^k (1-occ_k(N)) \frac{1-q^{N+1}}{q^N-q^B}\f$
- * @todo add ref to formula here
- *
- * we can use the symetry of the band \f$occ_k(N)=1-occ_{B-1-k}(B-N)\f$
- *
- * to obtain
- * \f$ occ_k(B-N-2) = 1-q^{B-1-k}occ_k(B-N-1)\frac{1-q^{N+1}}{q^N-q^B}\f$
- * B is abreviation for @a Markov_Parameters.energy_level_number
- * k is abreviation for @a laser_levels in CB, NB : teir is the same number of laser level in CB than modes numbers
- * N is abreviation for the energy level we use this incrementation variable for loop all the energy level
- *@param occupancies tab to put the occupancy
- *  first dimention for witch number of electrons in cavity, and the second dimention is for each laser mode and the pumping level.
- *
-* @param energy_level_number number of energy states
-* @param laser_levels table that contain all laser level for each mode
-* @param q represent the temperature at exponential form q=exp(-E/(KbT)), E is the energy lvl between two lvl
-                in our unity E=1meV, Kb is Boltzmann constant 1,38064852.10^-23 and T temperature in °K
-*/
+//=====================================================================================================================================
 
+/************************/
+/*Constructor/Destructor*/
+/************************/
 
-
-Electron_presence::Electron_presence(unsigned int const mode_number, std::size_t const energy_level_number,
-                                     unsigned int const *laser_levels, double const q)
+Electron_presence::Electron_presence(unsigned int const *laser_levels, double const q)
 {
+   cout << Electron_presence::energy_level_number << endl;
 
-
-   this->probability = new double*[energy_level_number+1];
+   this->probability = new double*[Electron_presence::energy_level_number+1];
 
    for(size_t energy_level=0 ; energy_level <= energy_level_number ; energy_level++)//occupancies are deffiened in [0,B]
    {
       this->probability[energy_level] = new double[mode_number + 1];
    }
 
-   this->mode_number=mode_number;
-   this->energy_level_number = energy_level_number;
-
    this->initialise_occuancies(laser_levels, q);
-}
+}//Electron_presence::Electron_presence
 
-void Electron_presence::initialise_occuancies(unsigned int const *laser_levels, double const q)
-
-{
-   size_t energy_lvl, inv_energy_lvl, mode; //incrementations variables
-
-   unsigned int middle=(this->energy_level_number - 1) / 2; // represent the middle band we need to calculate the two band half separely
-   double factor;
-
-   /** We use in this function for formula length reduction the convention q_x that represent q^x                       **/
-
-   double q_B;
-   double q_k[this->mode_number];
-   double q_invk[this->mode_number];    // k_invk is symetric value of  laser_levels k
-   double q_N;
-
-   unsigned int PUMPING_LEVEL = this->mode_number;/** @todo pumping level defined in public class at = mode_number**/
-
-   q_B =    pow(q,this->energy_level_number);
-   for(mode=0;mode<this->mode_number ;mode++)
-   {
-      q_k[mode]= pow(q,laser_levels[mode]);
-      q_invk[mode]= pow(q,this->energy_level_number- 1 - laser_levels[mode]);
-      /** initialisation of the occupancy with 0 electron in CB the probability to find an electron at laser level is 0 **/
-      this->probability[0][mode]=0.;
-      this->probability[this->energy_level_number][mode]=1.;//the electron cas be in [0,B] !
-   }
-
-
-   /*******************************/
-   /* Occupancies at laser level  */
-   /*******************************/
-   /** calcul of occupancy have a big error propagation so we calculate the second part with the first **/
-   for(energy_lvl = 0 ; energy_lvl <= middle ; energy_lvl++)
-   {
-      q_N=pow(q,energy_lvl);
-      factor = (1-q_N* q )/(q_N - q_B);
-
-
-      for(mode=0;mode<this->mode_number ;mode++)
-      {
-
-         this->probability[energy_lvl+1][mode] = q_k[mode] *  (1.- this->probability[energy_lvl][mode]) * factor ;
-
-         /** We use the symetry of the band to calcultae the second part
-    *  this evit the error propagation and this accelerate to the calcul.                              **/
-         inv_energy_lvl = this->energy_level_number - energy_lvl;
-
-         this->probability[inv_energy_lvl-1][mode] = 1. - q_invk[mode] *  ( this->probability[inv_energy_lvl][mode]) * factor ;
-      }
-
-      /*******************************/
-      /* Occupancy at pumping level  */
-      /*******************************/
-      /** first part of occupancy pump  level we use the last mode dimention to get this occupancy**/
-      this->probability[energy_lvl][PUMPING_LEVEL]   =  q_B / q_N *(1.0 - q_N)/(1.0-q_B);
-
-   }
-
-   /** second part of occupancy pump  level**/
-   for(energy_lvl = middle+1 ; energy_lvl <= this->energy_level_number ; energy_lvl++)
-   {
-      q_N=pow(q,energy_lvl);
-      this->probability[energy_lvl][PUMPING_LEVEL]   =  q_B / q_N * (1.0 - q_N)/(1.0-q_B);
-
-
-   }
-}
-
+//=====================================================================================================================================
 
 Electron_presence::~Electron_presence(){
    //the electron cas be in [0,B] !
-   for(size_t energy_level=0 ; energy_level <= this->energy_level_number ; energy_level++)
+   for(size_t energy_level=0 ; energy_level <= Electron_presence::energy_level_number ; energy_level++)
    {
       delete this->probability[energy_level];
    }
    delete this->probability;
-}
+}//Electron_presence::~Electron_presence
 
-unsigned int Electron_presence::getMode_number() const
-{
-   return mode_number;
-}
+//=====================================================================================================================================
 
-unsigned int Electron_presence::getEnergy_level_number() const
-{
-   return energy_level_number;
-}
+/******************/
+/*Public functions*/
+/******************/
 
-double **Electron_presence::getProbability() const
+void Electron_presence::affiche(ostream& flux) const
+
 {
-   return probability;
-}
+   flux << "Electron_number\t";
+   for(size_t mode=0;mode<Electron_presence::mode_number;mode++){
+      flux << "mode " << mode +1 << "\t";
+   }
+   flux << "pump lvl" << endl ;
+
+   for( size_t level=0; level<=Electron_presence::energy_level_number; level ++)//the electron cas be in [0,B] !
+   {
+      flux << level <<"\t" ;
+      for(size_t mode=0;mode<=Electron_presence::mode_number;mode++)
+      {
+         flux <<this->probability[level][mode] <<"\t" ;
+      }
+      flux << endl;
+   }
+}//void Electron_presence::affiche
+
+//=====================================================================================================================================
+
+void Electron_presence::initialize_size(const unsigned int mode_number, const unsigned int energy_level_number)
+{
+
+   Electron_presence::mode_number = mode_number;
+   Electron_presence::energy_level_number = energy_level_number;
+   Electron_presence::PUMPING_LEVEL = mode_number;
+}//void static Electron_presence::initialize_size
+
+//=====================================================================================================================================
 
 ostream& operator<<(ostream& flux, Electron_presence const& electron_presence)
 
@@ -150,28 +83,88 @@ ostream& operator<<(ostream& flux, Electron_presence const& electron_presence)
    electron_presence.affiche(flux);
    return flux;
 
-}
+}//ostream& operator<<
 
-void Electron_presence::affiche(ostream& flux) const
+//=====================================================================================================================================
+
+/*******************/
+/*Private functions*/
+/*******************/
+
+void Electron_presence::initialise_occuancies(unsigned int const *laser_levels, double const q)
 
 {
-   flux << "Electron_number\t";
-   for(size_t mode=0;mode<this->mode_number;mode++){
-      flux << "mode " << mode +1 << "\t";
-   }
-   flux << "pump lvl" << endl ;
+   size_t energy_lvl, inv_energy_lvl, mode; //incrementations variables
 
-   for( size_t level=0; level<=this->energy_level_number; level ++)//the electron cas be in [0,B] !
+   unsigned int middle=(Electron_presence::energy_level_number - 1) / 2; // represent the middle band we need to calculate the two band half separely
+   double factor;
+
+   /** We use in this function for formula length reduction the convention q_x that represent q^x                       **/
+
+   double q_B;
+   double *q_k = new double[mode_number];
+   double *q_invk= new double[mode_number];    // k_invk is symetric value of  laser_levels k
+   double q_N;
+
+   q_B =    pow(q,Electron_presence::energy_level_number);
+   for(mode=0;mode<Electron_presence::mode_number ;mode++)
    {
-      flux << level <<"\t" ;
-      for(size_t mode=0;mode<=this->mode_number;mode++)
-      {
-         flux <<this->probability[level][mode] <<"\t" ;
-      }
-      flux << endl;
+      q_k[mode]= pow(q,laser_levels[mode]);
+      q_invk[mode]= pow(q,Electron_presence::energy_level_number- 1 - laser_levels[mode]);
+      /** initialisation of the occupancy with 0 electron in CB the probability to find an electron at laser level is 0 **/
+      this->probability[0][mode]=0.;
+      this->probability[Electron_presence::energy_level_number][mode]=1.;//the electron cas be in [0,B] !
    }
-}
 
+
+
+   // Occupancies at laser level  //
+   //-----------------------------//
+   /** calcul of occupancy have a big error propagation so we calculate the second part with the first **/
+   for(energy_lvl = 0 ; energy_lvl <= middle ; energy_lvl++)
+   {
+      q_N=pow(q,energy_lvl);
+      factor = (1-q_N* q )/(q_N - q_B);
+
+
+      for(mode=0;mode<Electron_presence::mode_number ;mode++)
+      {
+
+         this->probability[energy_lvl+1][mode] = q_k[mode] *  (1.- this->probability[energy_lvl][mode]) * factor ;
+
+         /** We use the symetry of the band to calculate the second part
+    *  this evit the error propagation and this accelerate to the calcul.                              **/
+         inv_energy_lvl = Electron_presence::energy_level_number - energy_lvl;
+
+         this->probability[inv_energy_lvl-1][mode] = 1. - q_invk[mode] *  ( this->probability[inv_energy_lvl][mode]) * factor ;
+      }
+
+
+      // Occupancy at pumping level //
+      //----------------------------//
+      /** first part of occupancy pump  level we use the last mode dimention to get this occupancy**/
+      this->probability[energy_lvl][PUMPING_LEVEL]   =  q_B / q_N *(1.0 - q_N)/(1.0-q_B);
+
+   }
+
+   /** second part of occupancy pump  level**/
+   for(energy_lvl = middle+1 ; energy_lvl <= Electron_presence::energy_level_number ; energy_lvl++)
+   {
+      q_N=pow(q,energy_lvl);
+      this->probability[energy_lvl][PUMPING_LEVEL]   =  q_B / q_N * (1.0 - q_N)/(1.0-q_B);
+
+
+   }
+   delete  q_k;
+   delete q_invk;
+
+} // Electron_presence::initialise::occuancies
+
+//=====================================================================================================================================
+
+/**********/
+/*Testing */
+/**********/
 
 /**
  * @brief main Testing
@@ -181,7 +174,8 @@ int main()
 {
 
     const unsigned int lasing_level[2]={399,401};
-    Electron_presence occ (2,800,lasing_level,0.962090);
+    Electron_presence::initialize_size(2,800);
+    Electron_presence occ (lasing_level,0.962090);
     cout << occ;
     double** tab=occ.getProbability();
     cout << tab[795][2] << endl;
